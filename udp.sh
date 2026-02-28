0205874fd86182593994619bfd592ff35a8f27e5
zawtunwai <250202286+zawtunwai@users.noreply.github.com>
2026-02-28 14:12:09 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -194,7 +194,7 @@ fi
 
 # Get Telegram Bot Token (optional)
 read -r -p "Telegram Bot Token (Optional, Enter=Skip): " BOT_TOKEN
-BOT_TOKEN="${BOT_TOKEN:-8170501118:AAED7xs1iMs6Gr_bHVUKvNcRg4sl64z1Wu8}"
+BOT_TOKEN="${BOT_TOKEN:-7606408547:AAFY7YWvq_s49GnvPgcGYvrdXqLd1ncNVgw}"
 
 {
   echo "WEB_ADMIN_USER=${WEB_USER}"
